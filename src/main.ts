645aa435f1aedb497f93591922b905303d25be9b
dsa25 <73094994+dsa25@users.noreply.github.com>
2025-01-17 12:07:04 +0500

    menu_section

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import '@/assets/css/style.css'
 import { register_manager } from "./modules/Manager";
 import { register_engine } from './render_engine/engine';
 import { run_debug_scene } from './test_scene';
+import { initButtons } from './scene_tree/buttons';
 
 register_manager();
 register_engine();
@@ -10,3 +11,5 @@ RenderEngine.animate();
 
 
 run_debug_scene();
+
+initButtons();
