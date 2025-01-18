018967a5f1d322af41e9668fdc4325c1ee18fb29
dsa25 <73094994+dsa25@users.noreply.github.com>
2025-01-18 14:43:11 +0500

    tree search

--- src/scene_tree/buttons.ts
+++ src/scene_tree/buttons.ts
@@ -1,18 +1,30 @@
 export function initButtons() {
 
     window.addEventListener('click', (event: any) => {
-
+        
         const btn_menu = event.target.closest(".btn_menu");
         if (btn_menu) {
             const menu_section = btn_menu.closest(".menu_section");
             menu_section.classList.toggle("active")
         }
-
+        
         const tree__btn = event.target.closest(".tree__btn");
         if (tree__btn) {
             tree__btn.classList.toggle("active")
         }
+        
+    });
+    
+    window.addEventListener('keyup', (event: any) => {
 
+        const field = event.target.closest(".poisk_us");
+        const links =  document.querySelectorAll(`#wr_tree li a`);
+        links.forEach((a:any) => {
+            a.classList.remove("color_green")
+            if(field.value.trim().length > 0 && a.textContent.includes(field.value.trim()))
+                a.classList.add("color_green")
+        });
+        
     });
 
 }
